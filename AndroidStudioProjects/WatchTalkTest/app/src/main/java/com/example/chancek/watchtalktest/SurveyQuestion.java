0e01e0d7368f939c50fdcc2183af4af5d4c00219
Chance Kuo <clkuo888@gmail.com>
2018-11-15 14:15:44 -0800

    Minor tts language fix

--- AndroidStudioProjects/WatchTalkTest/app/src/main/java/com/example/chancek/watchtalktest/SurveyQuestion.java
+++ AndroidStudioProjects/WatchTalkTest/app/src/main/java/com/example/chancek/watchtalktest/SurveyQuestion.java
@@ -47,7 +47,7 @@ public class SurveyQuestion extends WearableActivity {
             public void onInit(int status) {
                 // Ask a question
                 if(status == TextToSpeech.SUCCESS){
-                    int result=tts.setLanguage(Locale.UK);
+                    int result=tts.setLanguage(Locale.US);
                     if(result==TextToSpeech.LANG_MISSING_DATA ||
                             result==TextToSpeech.LANG_NOT_SUPPORTED){
                         Log.e("error", "This Language is not supported");
