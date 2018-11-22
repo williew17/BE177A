aed5748239d8afe33566196228234236411d8111
rdod9 <rdod@g.ucla.edu>
2018-11-21 19:27:15 -0800

    Added pincode changes

---
+++ AndroidStudioProjects/WatchTalkTest2/app/src/main/java/com/example/chancek/watchtalktest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.chancek.watchtalktest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+
+
+public class MainActivity extends WearableActivity {
+
+    //SurveyResponse Responses = new SurveyResponse();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+
+    public void startSurvey(View view)
+    {
+        Intent intent = new Intent(this, PinPage.class);
+        //intent.putExtra("Serialized_Responses", Responses);
+        startActivity(intent);
+    }
+
+
+}
