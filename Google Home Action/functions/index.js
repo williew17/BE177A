a1297d0fe890863a498d7b7370476c0ea08e225f
std-libchen <u6.30cl@gmail.com>
2019-02-05 14:10:06 -0800

    whoops im dum

--- Google Home Action/functions/index.js
+++ Google Home Action/functions/index.js
@@ -58,7 +58,7 @@ df.intent('Response', (conv, {num, phrase}) => {
             conv.ask("You have finished the assessment.");
             return api.testResults(token).then((results) => {
                 var file = new Buffer("test_name" + JSON.stringify(results), 'binary');
-                var keystring = token + ":" + output;
+                var keystring = token + ":" + output[0];
                 var opts = {Body: file, Bucket: "swellhomebucket", Key: keystring};
                 var complete = new Promise( function(resolve, reject) {
                     s3.putObject( opts, function(){});
